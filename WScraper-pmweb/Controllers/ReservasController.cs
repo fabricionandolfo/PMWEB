255bfd925dfe17b61e87d5f94dba62df5d8f609a
nandolfofabricio <nandolfofabricio@gmail.com>
2023-10-02 03:04:22 -0300

    intalando selenium

--- WScraper-pmweb/Controllers/ReservasController.cs
+++ WScraper-pmweb/Controllers/ReservasController.cs
@@ -1,4 +1,9 @@
 ﻿using HtmlAgilityPack;
+using java.sql;
+using javax.xml.xpath;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -64,7 +69,7 @@ namespace WScraper_pmweb.Controllers
 
         public void CriarCache(DateTime checkin, DateTime checkout, int quantidadeAdulto, int quantidadeCrianca) 
         {
-            Reserva reserva= new Reserva();
+			Reserva reserva = new Reserva();
 			reserva.Checkin = checkin;
 			reserva.Checkout = checkout;
 			reserva.QuantidadeAdulto = quantidadeAdulto;
@@ -73,11 +78,40 @@ namespace WScraper_pmweb.Controllers
 			Quarto quarto = new Quarto();
 			reserva.Quartos = new List<Quarto> { quarto };
 
-			string pagina = ("https://testautomation.letsbook.com.br/D/Reserva?checkin="+ checkin.Day+ "%2"+ checkin.Month+ "%2F"+checkin.Year+"&checkout="+ checkout.Day+ "%2F"+ checkout.Month+ "%2F"+ checkout.Year+ "&hotel=14&adultos="+ quantidadeAdulto + "&criancas="+ quantidadeCrianca);
+			IWebDriver driver = new ChromeDriver();
+
+			// Navegar para a página da web
+			driver.Navigate().GoToUrl("https://testautomation.letsbook.com.br/D/Reserva?checkin=02%2F10%2F2023&checkout=03%2F10%2F2023&cidade=&hotel=14&adultos=1&criancas=&destino=Hotel+QA+Absoluto&promocode=&tarifa=&mesCalendario=&_ga=&_gl=&_gcl="); // Substitua pela URL que deseja acessar
+
+			// Aguarde até que a página seja completamente carregada (incluindo o JS)
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+			wait.Until(webDriver => ((IJavaScriptExecutor)webDriver).ExecuteScript("return document.readyState").Equals("complete"));
+
+			// Obtenha o conteúdo HTML da página carregada
+			string pageSource = driver.PageSource;
+
+			// Use o HtmlAgilityPack para fazer scraping do conteúdo
+			HtmlDocument doc = new HtmlDocument();
+			doc.LoadHtml(pageSource);
+
+
 
 			
 
-            
+			
+
+
+
+			foreach (HtmlNode node in htmlDocument.DocumentNode.SelectNodes(xpath:"//*[@id=\"tblAcomodacoes\"]/tbody").ToList())
+			{
+				if (node.Attributes.Count > 0)
+				{
+					quarto.Nome = node.Descendants().First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Equals("quartoNome")).InnerText;
+				}
+
+			}
+
+
 
 		}
         public List<Reserva> buscaReservas(DateTime checkin, DateTime checkout, int quantidadeAdulto, int quantidadeCrianca) 
